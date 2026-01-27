c22d76b0ffe2a6ac7c9819b2b6514e0873f42a3b
GrigoriyShamodi <grigoriy0811@gmail.com>
2026-01-27 21:59:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi! What's your name?");
+        System.out.println("Grigoriy Shamodi");
+        String name = "grigoriy";
+        System.out.println("HI " + name);
+
+
+
+        int x = 100000;
+        int y = 78000;
+        System.out.println(x - y);
+
+        // x = income y = spending
+    }
+}
