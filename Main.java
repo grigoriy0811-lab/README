f2921b6ef7a04f39af92c4939da64ece0017514a
GrigoriyShamodi <grigoriy0811@gmail.com>
2026-01-27 22:27:05 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Grioriy");
+        System.out.println("Grigoriy");
 
 
         int income = 100000;
