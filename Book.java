fa0439db180dd61dc18f8f7ecfa239c23ecbb6a0
GrigoriyShamodi <grigoriy0811@gmail.com>
2026-02-08 20:55:29 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,47 @@
+public class Book {
+    String title;
+    int yearOfRelease;
+    String author;
+    int pages;
+
+    public Book(String title, int yearOfRelease, String author, int pages) {
+        this.title = title;
+        this.yearOfRelease = yearOfRelease;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String toString() {
+        return "Книга: " + title + ", год: " + yearOfRelease +
+                ", aвтор: " + author + ", страниц: " + pages;
+    }
+
+    public boolean isBig() {
+        if(pages > 500) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean macher(String word) {
+        boolean inTitle = title.contains(word);
+        boolean inAuthor = author.contains(word);
+
+        if(inTitle == true || inAuthor == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int estimatePrice() {
+        int price = pages * 3;
+        if(price < 250) {
+            return 250;
+        } else {
+            return price;
+        }
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,83 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
-    public static int count1(int earnings, int spendings) {
-        int tax = (earnings - spendings) * 15 / 100;
-        if(tax >= 0) {
-            return tax;
-        } else {
-            return 0;
-        }
-    }
+    public static void main(String[] args) {
+        Book myBook = new Book("Шантарам", 2003, "Грегори Дэвид Робертс", 864);
+        System.out.println(myBook);
 
-    public static int count2(int earnings) {
-        int tax = earnings * 6 / 100;
-        if (tax >= 0) {
-            return tax;
-        } else {
-            return 0;
-        }
-    }
+        boolean isBookBig = myBook.isBig();
+        System.out.println("Книга Шантарам большая? " + isBookBig);
 
-    static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        boolean hasMather = myBook.macher("рам");
+        System.out.println("Есть ли слово 'рам' в название или имени автора? " + hasMather );
 
-        int earnings = 0;
-        int spendings = 0;
+        int priceBook = myBook.estimatePrice();
+        System.out.println("Цена книги " + priceBook + " рублей");
 
-        while (true) {
-            System.out.println(
-                    "Выберите операцию и введите её номер:\n" +
-                            "1. Добавить новый доход\n" +
-                            "2. Добавить новый расход\n" +
-                            "3. Выбрать систему налогообложения\n");
 
-            String input = scanner.nextLine();
-            if ("end".equals(input)) {
-                break;
-            }
-            int operation = Integer.parseInt(input);
-
-            switch (operation) {
-                case 1:
-                    System.out.println("Введите сумму дохода:");
-                    String income = scanner.nextLine();
-                    int money1 = Integer.parseInt(income);
-                    earnings += money1;
-                    break;
-                case 2:
-                    System.out.println("Введите сумму расходов:");
-                    String minus = scanner.nextLine();
-                    int money2 = Integer.parseInt(minus);
-                    spendings += money2;
-                    break;
-                case 3:
-                    int tax1 = count1(earnings, spendings);
-                    int tax2 = count2(earnings);
-
-                    if (tax1 < tax2) {
-                        System.out.println(
-                                "Мы советуем вам УСН доходы\n" +
-                                        "Ваш налог составит: " + tax1 + " рублей\n" +
-                                        "Налог на другой системе: " + tax2 + " рублей\n" +
-                                        "Экономия: " + (tax2 - tax1) + " рублей");
-                    } else if (tax2 < tax1) {
-                        System.out.println(
-                                "Мы советуем вам УСН доходы минус расходы\n" +
-                                        "Ваш налог составит: " + tax2 + " рублей\n" +
-                                        "Налог на другой системе: " + tax1 + " рублей\n" +
-                                        "Экономия: " + (tax1 - tax2) + " рублей");
-                    } else {
-                        System.out.println(
-                                "Можете выбрать любую систему налогообложения\n" +
-                                        "Налог составит: " + tax1 + " рублей");
-                    }
-                    break;
-
-                default:
-                    System.out.println("Такой операции нет");
-            }
-        }
-        System.out.println("Программа завершена!");
     }
 }
